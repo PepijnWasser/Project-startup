6b82be4ea008b54d0c02c710fe2352597cc6f01f
Pepijn Wasser <482906@student.saxion.nl>
2021-02-11 11:32:15 +0100

    changes

---
+++ project startup/Assets/scripts/Player controlls/scripts/ExpandBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Expand")]
+public class ExpandBall : Ability
+{
+    public float expandMultiplier = 2;
+    public float scaleTime = 0.5f;
+    private float currentScaleTime = 0;
+
+    bool expendAnimation = false;
+    Vector3 originalScale;
+
+    public override void HandleAbility(GameObject player, GameObject light)
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !expendAnimation)
+        {
+            expendAnimation = true;
+            originalScale = light.transform.localScale;
+        }
+        if (expendAnimation)
+        {
+            currentScaleTime += Time.deltaTime;
+            float percentage = currentScaleTime / scaleTime;
+            float val = Mathf.Sin(percentage * Mathf.PI);
+            float multiplier = val * expandMultiplier;
+            if (val > 1)
+            {
+                val = 1;
+            }
+
+            light.transform.localScale = originalScale + new Vector3(1, 1, 1) * multiplier;
+
+            if (percentage > 1)
+            {
+                expendAnimation = false;
+                currentScaleTime = 0;
+            }
+        }
+    }
+}
