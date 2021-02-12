83e47ca84097b17f4ea3a032c6807e4b7a80e9f3
Pepijn Wasser <482906@student.saxion.nl>
2021-02-12 11:55:26 +0100

    changes

---
+++ project startup/Assets/scripts/removable platforms/DeleteWhenInSphere.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteWhenInSphere : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    private void Update()
+    {
+        Vector2 bottomLeftCorner = new Vector2(transform.position.x, transform.position.y) - new Vector2(transform.localScale.x / 2, transform.localScale.y / 2);
+        Vector2 topRightCorner = new Vector2(transform.position.x, transform.position.y) + new Vector2(transform.localScale.x / 2, transform.localScale.y / 2);
+
+        Collider2D collider = Physics2D.OverlapArea(bottomLeftCorner, topRightCorner, layerMask);
+        if (collider != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
