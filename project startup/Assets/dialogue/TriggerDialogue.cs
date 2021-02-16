c5a5143f0c9f1be1b14601d629a9498cef013e2a
Pepijn Wasser <482906@student.saxion.nl>
2021-02-16 15:26:18 +0100

    dialogue

---
+++ project startup/Assets/dialogue/TriggerDialogue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour
+{
+    public string tag;
+    public dialogueManager[] dialogueManagers;
+    public int[] toWhatNumber;
+
+
+    private void Start()
+    {
+        if(dialogueManagers.Length != toWhatNumber.Length)
+        {
+            Debug.LogError("values are not equal");
+        }    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == tag)
+        {
+            Debug.Log("Player");
+            for (int i = 0; i < dialogueManagers.Length; i++)
+            {
+                dialogueManagers[i].PlayUntil(toWhatNumber[i]);
+            }
+        }
+    }
+}
---
+++ project startup/Assets/dialogue/dialogueManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour
+{
+    public GameObject textBox;
+    public float textSpeed;
+    public float nextStringDelay;
+
+    public string[] text;
+    string testString;
+
+    public Text textComponent;
+    int charsInString;
+
+    float secondcounter = 0;
+    int stringWeAreOn = 0;
+
+    int endNumber = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrepareNextString(text[stringWeAreOn]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(stringWeAreOn < endNumber)
+        {
+            textBox.SetActive(true);
+            if (textComponent.text.Length < charsInString)
+            {
+                secondcounter += Time.deltaTime;
+                if (secondcounter > textSpeed)
+                {
+                    secondcounter = 0;
+                    textComponent.text = textComponent.text + text[stringWeAreOn][textComponent.text.Length];
+                }
+            }
+            else if (textComponent.text.Length == charsInString)
+            {
+                secondcounter += Time.deltaTime;
+                if (secondcounter > nextStringDelay)
+                {
+                    if (stringWeAreOn < text.Length - 1)
+                    {
+                        stringWeAreOn += 1;
+                        PrepareNextString(text[stringWeAreOn]);
+                    }
+                    else
+                    {
+                        textBox.SetActive(false);
+                    }
+                }
+            }
+        }
+        else
+        {
+            textBox.SetActive(false);
+        }
+     
+    }
+
+    void PrepareNextString(string _string)
+    {
+        charsInString = _string.Length;
+        textComponent.text = "";
+        secondcounter = textSpeed;
+    }
+
+    public void PlayUntil(int number)
+    {
+        endNumber = number;
+    }
+}
